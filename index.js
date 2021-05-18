1d109ec6e4be6dd74f4e797993c04e13f6e16919
olgaTheCreator <curylo.olga@gmail.com>
2021-05-18 13:45:28 +0100

    uhu

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hi there!!!");
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener("click", this.start);
+    }
+    start = () => {
+        this.tick();
+        setInterval(this.tick, 1000);
+    }
+
+    tick = () => {
+        console.log('tick');
+    }
+}
+const durationInput = document.body.querySelector("#duration")
+const startButton = document.body.querySelector("#start");
+const pauseButton = document.body.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
