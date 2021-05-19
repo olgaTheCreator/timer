bb09d4aeb02ace365375acfa8263a30c6385c91f
olgaTheCreator <curylo.olga@gmail.com>
2021-05-19 10:48:57 +0100

    completed project

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const durationInput = document.body.querySelector("#duration")
 const startButton = document.body.querySelector("#start");
 const pauseButton = document.body.querySelector("#pause");
+const circle = document.body.querySelector('circle');
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
 
+let duration;;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-onStart() {
-    console.log('timer started');
+onStart(totalDuration) {
+    duration = totalDuration;
 },
-onTick() {
-    console.log('timer just clicked');
+onTick(timeRemaining) {
+    circle.setAttribute('stroke-dashoffset', 
+      perimeter * timeRemaining / duration - perimeter
+    );
 },
 onComplete() {
     console.log('timer is completed')
